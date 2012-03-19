c40a87a1b4115dcfb86058d3df007f904fe35c1f
Daniel Buhre <d.buhre@gmx.net>
2012-03-19 15:10:12 +0100

    Test von Bu...!!

--- Informate/src/www/webmatik/ch/InformateActivity.java
+++ Informate/src/www/webmatik/ch/InformateActivity.java
@@ -9,5 +9,6 @@ public class InformateActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        //Test von Buhre...!!
     }
 }
